2e90aa954263a3eafbed31b914d8733332220b15
satyabratapradhan <satyabratapradhann@gmail.com>
2025-02-03 23:57:55 +0530

    add error handling code

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 const port = 8080;
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride = require("method-override")
-const ejsMate = require("ejs-mate")
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const wrapAsync = require("./utils/wrapAsync.js");
 
 main().then(() =>{
-    console.log("connected to DB")
+    console.log("connected to DB");
 })
 .catch((err) =>{
-    console.log(err)
+    console.log(err);
 })
 
 async function main() {
@@ -26,56 +26,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("Hi! I am root")
-})
+app.get("/", wrapAsync((req, res) => {
+    res.send("Hi! I am root");
+}));
 
 //Index Rought
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListing = await Listing.find({});
     res.render("./listings/index.ejs", {allListing});
-});
+}));
 
 //New Route
-app.get("/listings/new",  (req, res) => {
+app.get("/listings/new", wrapAsync(  (req, res) => {
     res.render("./listings/new.ejs");
-})
+}));
 
 //show route
-app.get("/listings/:id",async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     const {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("./listings/show.ejs", {listing})
-})
+    res.render("./listings/show.ejs", {listing});
+}));
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync( async (req, res, next) => {
     const newListing = new Listing(req.body.listings);
     await newListing.save();
-    res.redirect("/listings")
-})
+    res.redirect("/listings");
+}));
 
 //edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", {listing});
-})
+}));
 
 //update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params; 
     await Listing.findByIdAndUpdate(id,req.body.listings);
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndDelete(id);
-    res.redirect("/listings")
-})
+    res.redirect("/listings");
+}));
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -91,5 +91,5 @@ app.delete("/listings/:id", async (req, res) => {
 // });
 
 app.listen(port, ()=>{
-    console.log(`server is litening the ${port}`)
+    console.log(`server is litening the ${port}`);
 })
--- models/listing.js
+++ models/listing.js
@@ -6,15 +6,27 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
-  description: String,
+  description: {
+    type: String,
+    required: true,
+  },
   image: {
     type: String, // Convert image field to a single string
     default: "https://th.bing.com/th/id/OIP.mmctUHU6M-TA0a89ipHLfAHaEK?rs=1&pid=ImgDetMain",
     set: (v) => v || "https://th.bing.com/th/id/OIP.mmctUHU6M-TA0a89ipHLfAHaEK?rs=1&pid=ImgDetMain",
   },
-  price: Number,
-  location: String,
-  country: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
